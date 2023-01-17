77d46d35ed4c433aaa5112cc1a77bc021a3a14b5
Abdesalam Almzayyen <35470559+almzayyen@users.noreply.github.com>
2023-01-16 20:47:10 -0500

    Update matrixRotation.py

--- matrixRotation.py
+++ matrixRotation.py
@@ -1,19 +1,53 @@
+def matrixRotation(matrix, r):
+    n, m = len(matrix), len(matrix[0])
 
+    for k in range(min(n, m)//2):
+        layer = []
+        rotation = r % (2 * (n+m-2-4*k))
 
-def rotate_matrix(matrix, r):
-    n = len(matrix)
-    m = len(matrix[0])
-    r = r % (n * m)  # in case r > n*m
-    # Transpose the matrix
-    for i in range(n):
-        for j in range(i, m):
-            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        #top
+        for i in range(k, m-k):
+            layer.append(matrix[k][i])
+        #right
+        for i in range(k+1, n-k-1):
+            layer.append(matrix[i][m-k-1])
+        #bottom
+        for i in range(m-k-1, k-1, -1):
+            layer.append(matrix[n-k-1][i])
+        #left
+        for i in range(n-k-2, k, -1):
+            layer.append(matrix[i][k])
 
-    # Reverse each row
-    for i in range(n):
-        matrix[i] = matrix[i][::-1]
+        l = 0
+        #top
+        for i in range(k, m-k):
+            matrix[k][i] = layer[(l+rotation) % len(layer)]
+            l += 1
+        #right
+        for i in range(k+1, n-k-1):
+            matrix[i][m-k-1] = layer[(l+rotation) % len(layer)]
+            l += 1
+        #bottom
+        for i in range(m-k-1, k-1, -1):
+            matrix[n-k-1][i] = layer[(l+rotation) % len(layer)]
+            l += 1
+        #left
+        for i in range(n-k-2, k, -1):
+            matrix[i][k] = layer[(l+rotation) % len(layer)]
+            l += 1
 
-    # Reverse the columns of the first r rows
-    for i in range(r):
-        matrix[i] = matrix[i][::-1]
-    return matrix
+    for i in matrix:
+        print(" ".join(map(str, i)))
+
+if __name__ == '__main__':
+    mnr = input().rstrip().split()
+    m = int(mnr[0])
+    n = int(mnr[1])
+    r = int(mnr[2])
+
+    matrix = []
+
+    for _ in range(m):
+        matrix.append(list(map(int, input().rstrip().split())))
+
+    matrixRotation(matrix, r)
