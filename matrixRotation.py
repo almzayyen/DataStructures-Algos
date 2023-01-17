c581c4d2039fc4d4f7ca3e24c86709881e17f41a
Abdesalam Almzayyen <35470559+almzayyen@users.noreply.github.com>
2023-01-16 20:48:56 -0500

    Update matrixRotation.py

--- matrixRotation.py
+++ matrixRotation.py
@@ -4,6 +4,11 @@ def matrixRotation(matrix, r):
     for k in range(min(n, m)//2):
         layer = []
         rotation = r % (2 * (n+m-2-4*k))
+        #This line calculates the number of positions by which the current layer of the matrix needs to be rotated. It does this by taking the input rotation value "r" and performing a modulo operation with the expression "(2 * (n+m-2-4*k))".
+
+        #The expression "2 * (n+m-2-4*k)" calculates the total number of elements in the current layer. It does this by first adding the number of rows "n" and the number of columns "m" of the matrix, then subtracting 2 to account for the corners of the matrix, and finally subtracting 4 times the current layer number "k" to account for the fact that each inner layer has 4 less elements than the previous one.
+
+        #The modulo operation with "r" ensures that the rotation value stays within the range of the number of elements in the current layer, so that the rotated position stays within the current layer.
 
         #top
         for i in range(k, m-k):
